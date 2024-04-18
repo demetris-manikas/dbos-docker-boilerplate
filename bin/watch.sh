2655bd2dc94f54b9a450fb42dac6b4f8b6d887d6
Demetris Manikas <dmanikas@admin.grnet.gr>
2024-04-18 11:02:22 +0300

    introduce services template

--- bin/watch.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-contents_changed () {
-    while read foo; do
-        npm --prefix ../app run build
-    done
-}
-
-inotifywait -mr --format '%:e %f' -e 'create,delete,close_write' ../app/src | contents_changed
