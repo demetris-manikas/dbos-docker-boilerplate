23e1a7129467574303f5c87a7286ac23454af829
Demetris Manikas <dmanikas@admin.grnet.gr>
2024-10-01 21:51:10 +0300

    Update deps

--- backend/src/middleware/auth/basic.ts
+++ backend/src/middleware/auth/basic.ts
@@ -6,7 +6,7 @@ export async function basicHttpAuthentication(
 ): Promise<HttpAuthenticationResult> {
     return Promise.resolve({
       // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment, @typescript-eslint/no-unsafe-member-access
-      authenticatedUser: ctx.koaContext?.header.username ?? '',
+      authenticatedUser: ctx.koaContext?.header.username as string ?? '',
       authenticatedRoles: ['user'],
     });
 }
